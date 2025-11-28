3e73736b842cd53015c39511caa175edd28f68b8
Venu-Developer <venu191202@gmail.com>
2025-11-28 14:51:10 +0530

    Update config.js

--- admin-emp-management/src/config.js
+++ admin-emp-management/src/config.js
@@ -1,6 +1,6 @@
 // export const API_URL = "http://192.168.29.54:5000";
 // export const API_URL = "http://192.168.29.78:5000";
-// export const API_URL = "https://hrms.aryuprojects.com";
+ export const API_URL = "https://hrms.aryuprojects.com";
 
 // export const API_URL = "https://hrms.aryutechnologies.com";
 
@@ -8,7 +8,7 @@
 
 // export const API_URL = "http://192.168.0.116:5000";
 
-export const API_URL = "http://localhost:5009"; //system        
+//export const API_URL = "http://localhost:5009"; //system        
 
 
 // export const API_URL = "http://192.168.0.116:5009";
