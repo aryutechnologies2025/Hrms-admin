7baf6d79d2d51551ba8c1d224057bbee469f615e
archanaradha95 <archanaradha95@gmail.com>
2026-01-30 12:49:55 +0530

    build updated for checking

--- admin-emp-management/src/config.js
+++ admin-emp-management/src/config.js
@@ -1,7 +1,7 @@
 // export const API_URL = "http://192.168.29.54:5000";
 // export const API_URL = "http://192.168.29.78:5000";
 
-export const API_URL = "https://hrms.aryuprojects.com";
+export const API_URL = "https://hrms.aryuprojects.com";   //staging
 
 // export const API_URL = "https://portal.aryutechnologies.com";
 
